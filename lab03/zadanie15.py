6b3bcadaeef3dfe3b37be586bfb9382b0d61e096
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-03-25 23:53:41 +0100

    lab03 zadanie 15

--- lab03/zadanie15.py
+++ lab03/zadanie15.py
@@ -15,13 +15,51 @@ def client_program():
     if success == 0:
         print("Successfully connected to the server.")
 
-        # message = f"zad13odp;src;{source_port};dst;{destination_port};data;{payload_decoded}"
+        protocol_version = int(ip_packet[0:1], 16)
 
-        # my_socket.send(message.encode())
+        print(f"Protocol version: {protocol_version}")
 
-        # server_answer = my_socket.recv(1024).decode()
+        protocol_type = int(ip_packet[18:20], 16)
 
-        # print(f"Server answer: {server_answer}")
+        print(f"Protocol type: {protocol_type}")
+
+        source_ip = ip_packet[24:32]
+        sourse_ip_str = f"{int(source_ip[0:2], 16)}.{int(source_ip[2:4], 16)}.{int(source_ip[4:6], 16)}.{int(source_ip[6:8], 16)}"
+
+        print(f"Source IP: {sourse_ip_str}")
+
+        destination_ip = ip_packet[32:40]
+        destination_ip_str = f"{int(destination_ip[0:2], 16)}.{int(destination_ip[2:4], 16)}.{int(destination_ip[4:6], 16)}.{int(destination_ip[6:8], 16)}"
+
+        print(f"Destination IP: {destination_ip_str}")
+
+        message = f"zad15odpA;ver;{protocol_version};srcip;{sourse_ip_str};dstip;{destination_ip_str};type;{protocol_type}"
+
+        my_socket.send(message.encode())
+
+        server_answer = my_socket.recv(1024).decode()
+
+        print(f"Server answer: {server_answer}")
+
+        if server_answer == "TAK":
+            source_port = int(ip_packet[40:44], 16)
+            destinatain_port = int(ip_packet[44:48], 16)
+
+            payload = ip_packet[104:]
+            payload_bytes = bytes.fromhex(payload)
+            payload_decoded = payload_bytes.decode()
+
+            print(f"Source port: {source_port}")
+            print(f"Destination port: {destinatain_port}")
+            print(f"Payload: {payload_decoded}")
+
+            another_message = f"zad15odpB;srcport;{source_port};dstport;{destinatain_port};data;{payload_decoded}"
+
+            my_socket.send(another_message.encode())
+
+            another_server_answer = my_socket.recv(1024).decode()
+
+            print(f"Server answer: {another_server_answer}")
 
     else:
         print('Connection failed!')
